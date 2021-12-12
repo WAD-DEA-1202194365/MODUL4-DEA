e601adcc7ee5427ff7762a1dbaf81733f41b3b68
dea <dhesihotang14@gmail.com>
2021-12-12 21:07:03 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php 
+
+ 
+
+$koneksi = mysqli_connect("localhost","root","","wad_modul4");
+
+ 
+
+// Check connection
+
+if (mysqli_connect_errno()){
+
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+
+}
+
+ 
+
+?>
